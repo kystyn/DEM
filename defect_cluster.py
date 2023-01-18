d0d5b0bfa343546d766225cfa48cc4a2125d0e3c
kystyn <konstantindamaskinskiy@gmail.com>
2023-01-18 17:25:42 +0600

    Programs to process results DEM experiments

---
+++ defect_cluster.py
@@ -0,0 +1,237 @@
+from math import atan2
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+import argparse
+import csv
+from pathlib import Path
+from tqdm import tqdm
+from functools import cmp_to_key
+import matplotlib.pyplot as plt
+
+
+def binary_search(arr, elem):
+    total = len(arr)
+    l = 0
+    r = total
+
+    while r - l > 1:
+        cur = (l + r) // 2
+        if elem < arr[cur]:
+            r = cur
+        elif elem > arr[cur]:
+            l = cur
+        else:
+            return cur, arr[cur]
+    return r, arr[r]
+
+
+def list_to_str(l: list) -> list[str]:
+    for i in range(len(l)):
+        l[i] = str(l[i])
+    return l
+
+
+def cmp_format(x, y):
+    if x < y:
+        return -1
+    if x > y:
+        return 1
+    return 0
+
+
+def compare_bonds(bond1, bond2):
+    x1, y1, z1 = bond1[0], bond1[1], bond1[2]
+    x2, y2, z2 = bond2[0], bond2[1], bond2[2]
+
+    if abs(z1 - z2) > 1e-6:
+        return cmp_format(z1, z2)
+
+    r1 = (x1 ** 2 + y1 ** 2) ** 0.5
+    r2 = (x2 ** 2 + y2 ** 2) ** 0.5
+
+    if abs(r1 - r2) > 1e-6:
+        return cmp_format(r1, r2)
+    
+    phi1 = atan2(y1, x1)
+    phi2 = atan2(y2, x2)
+
+    return cmp_format(phi1, phi2)
+
+
+def make_clusters(
+        filename: str, output_dir: str, dist_thr: float, 
+        step: float, integral: bool, show: bool, large: bool):
+    xlist = []
+    ylist = []
+    zlist = []
+    deathtlist = []
+    num_clusters_to_show = 10
+
+    print(f'Loading {filename}')
+    with open(filename, 'r') as f:
+        reader = csv.DictReader(f, delimiter=' ')
+
+        for row in reader:
+            xlist.append(float(row['BondX,mm']))
+            ylist.append(float(row['BondY,mm']))
+            zlist.append(float(row['BondZ,mm']))
+            deathtlist.append(float(row['DeathT,s']))
+
+    time_points = []
+    with open(str(Path(filename).parent /'time_points.txt'), 'r') as tpf:
+        l = tpf.readline().split(' ')[:-1]
+        for el in l:
+            time_points.append(float(el))
+
+    time_points = np.linspace(time_points[0], time_points[-1], int((time_points[-1] - time_points[0]) / step) + 1)
+
+    data = {tp: [] for tp in time_points} # key: timepoint value: [np.array([x, y, z])]
+    for cx, cy, cz, t in zip(xlist, ylist, zlist, deathtlist):
+        round_t_idx = min(binary_search(time_points, t)[0], len(time_points) - 1)
+        round_t = time_points[round_t_idx]
+        data[round_t].append(np.array([cx, cy, cz]))
+
+    if integral:
+        for idx, t in enumerate(time_points[1:]):
+            data[t] += data[time_points[idx]]
+
+    print('Loading finished')
+
+    #for t in tqdm(data, desc='Conversion to numpy', total=len(data)):
+    #    data[t] = np.asarray(data[t])
+    
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+    sep = '\t'
+    with open(str(Path(output_dir) / 'statistics.txt'), 'w') as sf:
+        sf.write(sep.join(['Time (end), s', 'Cluster count', 'Max size', '\n']))
+        for t, bonds in tqdm(data.items(), desc='Clustering', total=len(data)):
+            with open(str(Path(output_dir) / 'cluster_{:.6f}.txt').format(t), 'w') as f:
+                f.write(sep.join(['X,mm', 'Y,mm', 'Z,mm', 'D,mm', 'N']) + '\n')
+                # try-catch is KOSTYL'
+                try:
+                    #part_len = 90000
+                    #num_parts = (len(bonds) + part_len - 1) // part_len
+                    part_len = len(bonds)
+                    num_parts = 1
+
+                    bonds.sort(key=cmp_to_key(compare_bonds))
+
+                    fig = plt.figure()
+                    ax = plt.axes(projection='3d')  
+                    ax.set_label(f'Time {t}')
+                    
+                    max_cluster_len = 0
+                    cluster_cnt = 0
+                    is_large = False
+
+                    for part in range(num_parts):
+                        if len(bonds) == 0:
+                            continue
+                        clustering = AgglomerativeClustering(
+                            n_clusters=None,
+                            linkage='single',
+                            memory=str(Path.cwd()), 
+                            distance_threshold=dist_thr,
+                            compute_full_tree=True).fit(bonds[part * part_len:min((part + 1) * part_len, len(bonds))])
+                        
+                        clustered = [[] for _ in range(clustering.n_clusters_)]
+
+                        for label, elem in zip(clustering.labels_, bonds):
+                            clustered[label].append(elem)
+
+                        for label in range(len(clustered)):
+                            clustered[label] = np.asarray(clustered[label])
+
+                        ordered_labels = sorted(list(range(clustering.n_clusters_)), 
+                                        key=lambda idx: clustered[idx].shape[0])
+
+                        max_cluster  = clustered[ordered_labels[-1]]
+                        max_cluster_len = max(max_cluster_len, len(max_cluster))
+                        cluster_cnt += clustering.n_clusters_
+                        np.savetxt(f'{output_dir}/max_cluster_part{part}_'+'{:5f}.txt'.format(t), max_cluster,
+                                    header='X\tY\tZ', comments='')
+                        
+                        #center_x = [0] * len(clustering.n_clusters_) # idx -- label
+                        #center_y = [0] * len(clustering.n_clusters_)
+                        #center_z = [0] * len(clustering.n_clusters_)
+                        #diameters = [0] * len(clustering.n_clusters_)
+
+                        for label, elements in enumerate(clustered):
+                            # Calculate center of cluster
+                            cx = 0
+                            cy = 0
+                            cz = 0
+                            for elem in elements:
+                                cx += elem[0]
+                                cy += elem[1]
+                                cz += elem[2]
+                            cx /= len(elements)
+                            cy /= len(elements)
+                            cz /= len(elements)
+
+                            # Calculate diameter of cluster
+                            furthest_element = max(elements, 
+                                key=lambda elem: (elem[0] - cx) ** 2 + (elem[1] - cy) ** 2 + (elem[2] - cz) ** 2)
+                            d = ((furthest_element[0] - cx) ** 2 + (furthest_element[1] - cy) ** 2 + (furthest_element[2] - cz) ** 2) ** 0.5
+
+                            #center_x[label] = cx
+                            #center_y[label] = cy
+                            #center_z[label] = cz
+                            #diameters[label] = d
+                            f.write(sep.join(list_to_str([cx, cy, cz, d, clustered[label].shape[0]])) + '\n')
+
+                            if label in ordered_labels[-num_clusters_to_show:]:
+                                #vertices_sorted = clustered_conv_hull[label].vertices.tolist()
+                                vertices_sorted = list(range(clustered[label].shape[0]))
+                                vertices_sorted.sort(key=cmp_to_key(
+                                    lambda idx1, idx2:
+                                        compare_bonds(clustered[label][idx1], clustered[label][idx2]))
+                                )
+                                step = max(1, len(vertices_sorted) // 2000)
+                                if step > 1:
+                                    is_large = True
+                                vertices_sorted = np.asarray(vertices_sorted[::step])
+                                ax.scatter3D(clustered[label][vertices_sorted, 0],
+                                             clustered[label][vertices_sorted, 1],
+                                             clustered[label][vertices_sorted, 2])
+
+                    if show:
+                        if not large:
+                            plt.show()
+                        else: #if large
+                            if is_large:
+                                plt.show()
+                    fig.savefig(f'{output_dir}/img_' + '{:5f}.png'.format(t) )
+                    plt.close()
+                    sf.write(sep.join(list_to_str([t, cluster_cnt, max_cluster_len, '\n'])))
+
+                except Exception as e:
+                    print(f'Exception occured: {e}')
+        
+
+def main():
+    parser = argparse.ArgumentParser(description='Musen defect clusteriser')
+    parser.add_argument('--input', '-i', default='bonds_preprocessed.txt', type=str,
+                        help='Preprocessed bond file')
+    parser.add_argument('--output', '-o', default='cluster_test', type=str,
+                        help='Directory for output with clustered bonds')
+    parser.add_argument('--distance', '-d', default=0.2, type=float,
+                        help='Distance threshold for clustering')
+    parser.add_argument('--step', '-s', default=0.0005, type=float,
+                        help='Time interval to build clusters')
+    parser.add_argument('--partial', action='store_false',
+                        help='Partial storage of bonds')
+    parser.add_argument('--show', action='store_true',
+                        help='Show plots')
+    parser.add_argument('--large', action='store_true',
+                        help='Show plots with only large clusters')
+
+    args = parser.parse_args()
+    make_clusters(
+        args.input, args.output, args.distance,
+        args.step, args.partial, args.show, args.large)
+
+
+if __name__ == '__main__':
+    main()
---
+++ defect_processor.py
@@ -0,0 +1,273 @@
+from tqdm import tqdm
+import argparse
+from pathlib import Path
+
+
+def load_particles(particle_filename: str):
+    particles = {} # key: id value: (radius, [positions])
+
+    time_marker = '2'
+    radius_marker = '5'
+    coord_marker = '12'
+    events = -1
+
+    with open(particle_filename, 'r') as f:
+        lines = f.readlines()
+        for idx, l in tqdm(enumerate(lines), desc='Reading particle file', total=len(lines)):
+            data = l.split(' ')
+            if data[0] == '\n':
+                continue
+            if data[0] != '0':
+                raise RuntimeError(f'Line {idx}: First column is expected to be zero, got {data[0]}')
+            id = int(data[1])
+            radius = -1
+            positions = [] # key -- time point, value -- (x, y, z)
+            idx = 2
+            while idx < len(data):
+                if data[idx] == radius_marker:
+                    radius = float(data[idx + 1])
+                    idx += 2
+                elif data[idx] == time_marker:
+                    time = float(data[idx + 1])
+                    if data[idx + 2] != coord_marker:
+                        raise RuntimeError(f'Line {idx}: {coord_marker} is expected after <{time_marker} tp>. Got {data[idx + 2]}')
+                    pos = (float(data[idx + 3]) * 1000, float(data[idx + 4]) * 1000, float(data[idx + 5]) * 1000)
+                    idx += 6
+                    positions.append((time, pos))
+                else:
+                    idx += 1
+            if events != -1:
+                if len(positions) != events:
+                    raise RuntimeError(f'Line {idx}: Different particles have different events count')
+            else:
+                events = len(positions)
+            if radius == -1:
+                raise RuntimeError(f'Line {idx}: Radius was not mentioned')
+            particles[id] = (radius, positions)
+    return particles, events
+
+
+def load_bonds(bond_filename: str):
+    # key: id value: [begin_particle_id, end_particle_id, death tp, {time: (x, y, z)}, mtl]
+    bonds = {}
+    
+    time_marker = '2'
+    ends_marker = '5'
+    mtl_marker = '23'
+    ttl_marker = '24'
+    coord_marker = '12'
+
+    events = -1
+    has_all_coords = True
+    has_coords = True
+    with open(bond_filename, 'r') as f, open('defect_processor_log.txt', 'w') as log:
+        lines = f.readlines()
+        for l in tqdm(lines, desc='Reading bond file', total=len(lines)):
+            data = l.split(' ')
+            if data[0] == '\n':
+                continue
+            if data[0] != '0':
+                raise RuntimeError(f'Line {idx}: First column is expected to be zero, got {data[0]}')
+            id = int(data[1])
+            begin_id = -1
+            end_id = -1
+            death_tp = -1
+            idx = 2
+            mtl = None
+            x = None
+            y = None
+            z = None
+            time = None
+            times = {} # key: time value: (x, y, z)
+            cur_events = 0
+            while idx < len(data):
+                if data[idx] == ends_marker:
+                    begin_id, end_id = int(data[idx + 1]), int(data[idx + 2])
+                    idx += 3
+                elif data[idx] == ttl_marker:
+                    start_tp, death_tp = float(data[idx + 1]), float(data[idx + 2])
+                    if start_tp != 0:
+                        raise RuntimeError(f'Line {idx}: Particle was born unexpected: born time is {start_tp}, expected 0')
+                    idx += 3
+                elif data[idx] == time_marker:
+                    time = float(data[idx + 1])
+                    cur_events += 1
+                    idx += 2
+                elif data[idx] == coord_marker:
+                    x, y, z = float(data[idx + 1]), float(data[idx + 2]), float(data[idx + 3])
+                    # suppose coordinates appear after time
+                    times[time] = (x * 1000, y * 1000, z * 1000)
+                    idx += 4
+                elif data[idx] == mtl_marker:
+                    mtl = data[idx + 1]
+                    idx += 2
+                else:
+                    idx += 1
+            if begin_id == -1 or end_id == -1 or death_tp == -1:
+                raise RuntimeError(f'Line {idx}: Expected begin and end particle id, death time point. Got {begin_id}, {end_id}, {death_tp}')
+            if events != -1:
+                if cur_events != events:
+                    raise RuntimeError(f'Line {idx}: Different particles have different events count')
+            else:
+                events = cur_events
+            has_coords = len(times) > 0
+            has_all_coords &= has_coords
+            bonds[id] = [begin_id, end_id, death_tp, times if has_coords else None, mtl]
+    return bonds, {'events': events, 'has_all_coords': has_all_coords}
+
+
+def is_particle_in_ROI(radius: float, height: float, bond_length: float, 
+                        x: float, y: float, z: float) -> bool:
+    if not (-(height + bond_length) <= z < height + bond_length):
+        return False
+    if x * x + y * y > (radius + bond_length) ** 2:
+        return False
+    return True
+
+
+def binary_search(arr, elem):
+    total = len(arr)
+    l = 0
+    r = total
+
+    while r - l > 1:
+        cur = (l + r) // 2
+        if elem < arr[cur]:
+            r = cur
+        elif elem > arr[cur]:
+            l = cur
+        else:
+            return cur, arr[cur]
+    return l, arr[l]
+
+
+def list_to_str(l: list) -> list[str]:
+    for i in range(len(l)):
+        l[i] = str(l[i])
+    return l
+
+
+def process_particles_bonds(particles: dict, bonds: dict, radius: float,
+                            height: float, bond_length: float, out_filename: str):
+    sep = ' '
+    # add death time to all particles if they flew out of sample
+    particle_death = {id: 2 ** 100 for id in particles}
+    for id, particle in tqdm(particles.items(), desc='Evaluate particle death time. Step #1', total=len(particles)):
+        rad, positions = particle
+        prev_time = 0
+        for time, position in positions:
+            if not is_particle_in_ROI(radius, height, bond_length, *position):
+                particle_death[id] = prev_time
+                break
+            prev_time = time
+
+    # re-evaluate death time of bonds according to death times of particles
+    for id, bond in tqdm(bonds.items(), desc='Evaluate particle death time. Step #2',total=len(bonds)):
+        begin_id, end_id, death_tp = bond[0], bond[1], bond[2]
+        # assign new death time as minimum of all death times
+        bond[2] = min(particle_death[begin_id], particle_death[end_id], death_tp)
+
+    with open(out_filename, 'w') as f, open(str(Path(out_filename).parent /'time_points.txt'), 'w') as tpf:
+        time_points = []
+        particle0 = particles[next(iter(particles))][1]
+        for tp, position in particle0:
+            time_points.append(tp)
+        time_points.sort()
+
+        for tp in time_points:
+            tpf.write(f'{tp} ')
+        tpf.write('\n')
+
+        f.write(sep.join(['BondId', 'BondX,mm', 'BondY,mm', 'BondZ,mm', 'DeathT,s',
+                'BeginParticleId', 'BeginParticleX,mm', 'BeginParticleY,mm', 'BeginParticleZ,mm',
+                'EndParticleId', 'EndParticleX,mm', 'EndParticleY,mm', 'EndParticleZ,mm']) + '\n')
+
+        for id, bond in tqdm(bonds.items(), desc='Save result to file', total=len(bonds)):
+            begin_id, end_id, death_tp = bond[0], bond[1], bond[2]
+
+            if death_tp > time_points[-1] - 1e-5:
+                continue
+
+            tp_idx = binary_search(time_points, death_tp)[0]
+
+            begin_radius, begin_positions = particles[begin_id]
+            begin_coord = begin_positions[tp_idx][1]
+
+            end_radius, end_positions = particles[end_id]
+            end_coord = end_positions[tp_idx][1]
+
+            middle_coord = tuple((begin_coord[i] + end_coord[i]) / 2 for i in range(3))
+            
+            output = sep.join(list_to_str([id, *middle_coord, death_tp, begin_id, *begin_coord, end_id, *end_coord]))
+            f.write(output + '\n')
+
+
+def process_bonds(bonds: dict, radius: float, height: float, bond_length: float, out_filename: str):
+    sep = ' '
+    with open(out_filename, 'w') as f, open(str(Path(out_filename).parent /'time_points.txt'), 'w') as tpf:
+        time_points = []
+        bonds0 = bonds[next(iter(bonds))][3]
+        for tp in bonds0:
+            time_points.append(tp)
+        time_points.sort()
+
+        for tp in time_points:
+            tpf.write(f'{tp} ')
+        tpf.write('\n')
+
+        f.write(sep.join(['BondId', 'BondX,mm', 'BondY,mm', 'BondZ,mm', 'DeathT,s',
+                'BeginParticleId', 'EndParticleId', 'Material']) + '\n')
+
+        for id, bond in tqdm(bonds.items(), desc='Save result to file', total=len(bonds)):
+            begin_id, end_id, death_tp, time_to_coord, mtl = bond[0], bond[1], bond[2], bond[3], bond[4]
+
+            if death_tp > time_points[-1] - 1e-5:
+                continue
+
+            tp = binary_search(time_points, death_tp)[1]
+            coord = time_to_coord[tp]
+            if is_particle_in_ROI(radius, height, bond_length, *coord):
+                output = sep.join(list_to_str([id, *coord, death_tp, begin_id, end_id, mtl if mtl is not None else '']))
+                f.write(output + '\n')
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Musen defect processor')
+
+    parser.add_argument('--particle', '-p', default='particles.txt', type=str,
+                        help='Particle file')
+    parser.add_argument('--bond', '-b', default='bonds.txt', type=str,
+                        help='Bonds file')
+    parser.add_argument('--compute_coords', '-c', action='store_true',
+                        help='Compute bond coordinates with own functionality')
+    parser.add_argument('--radius', '-r', default=5.0, type=float,
+                        help='Sample radius')
+    parser.add_argument('--height', default=20.0, type=float,
+                        help='Sample height')
+    parser.add_argument('--length', '-l', default=0.3, type=float,
+                        help='Average bond length')
+    parser.add_argument('--out', '-o', default='bonds_preprocessed.txt', type=str,
+                        help='Output file')                        
+
+    args = parser.parse_args()
+    try:
+        if args.compute_coords:
+            particles, particles_events = load_particles(args.particle)
+            bonds, bonds_events = load_bonds(args.bond)
+
+            if particles_events != bonds_events['events']:
+                raise RuntimeError(f'Bond and particles files are mismatched. '\
+                                f'Got {particles_events} particles events, {bonds_events} bonds events')
+            process_particles_bonds(particles, bonds, args.radius, args.height, args.length, args.out)
+        else:
+            print('WARNING: particles file is not needed in this mode')
+            bonds, bonds_events = load_bonds(args.bond)
+            if not bonds_events['has_all_coords']:
+                raise RuntimeError('Some bonds do not have coords')
+            process_bonds(bonds, args.radius, args.height, args.length, args.out)
+    except Exception as e:
+        print(f'Exception occured: {e}')
+
+
+if __name__ == '__main__':
+    main()
